a230344de44639dac3eaf10fc3e633304765a4e3
cameronae <138916869+cameronae@users.noreply.github.com>
2025-02-10 22:54:52 -0500

    Update and rename script.js to yes.html

--- script.js
+++
@@ -1,8 +0,0 @@
-// Add interactivity to the buttons
-document.querySelector('.button.yes').addEventListener('click', () => {
-    alert('Yay! Happy Valentine\'s Day! 💖');
-});
-
-document.querySelector('.button.no').addEventListener('click', () => {
-    alert('Aww, maybe next time! 😢');
-});
